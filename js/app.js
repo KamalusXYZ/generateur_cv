dc616880171b426dbc70306f1413571b8e0ccb23
kamalus <formation.kamal.haddou@gmail.com>
2022-02-24 16:26:44 +0100

    commit init

--- js/app.js
+++ js/app.js
@@ -1,20 +1,131 @@
-let personal = {
+let skill = {
   template: `
-  <div>
-  <h2>Données personnels</h2>
-  Nom:  {{nom}}
+  <div class=box3 >
+  <h2>Mes Compétences</h2>
+  
+  <ul>
+    <li v-for="skill of skills"> {{skill.name}}</li>
+  </ul>
+  
   </div>
+
   `,
-  props: ["nom"],
+  props: ["skills"],
   data: function () {
     return {
 
-      nom: null,
+
+    }
+  },
+  methods: {
+
+
+
+
+  }
+
+
+
+
+}
+
+
+
+let exp = {
+  template: `
+  <div class=box2 >
+  <h2>Mes Expériences</h2>
+  
+  <ul>
+    <li v-for="job of jobs"> {{job.name}} {{job.start}} {{job.end}}</li>
+  </ul>
+  
+  </div>
+
+  `,
+  props: ["jobs"],
+  data: function () {
+    return {
+
+
+    }
+  },
+  methods: {
+
+
+
+
+  }
+
+
+
+
+}
+
+
+let formation = {
+  template: `
+  <div class=box2 >
+  <h2>Mes formations</h2>
+  
+  <ul>
+    <li v-for="school of schools"> {{school.name}} {{school.start}} {{school.end}}</li>
+  </ul>
+  
+  </div>
+
+  `,
+  props: ["schools"],
+  data: function () {
+    return {
+
+
+    }
+  },
+  methods: {
+
+
+
+
+  }
+
+
+
+
+}
+
+
+let personal = {
+  template: `
+  <div class="box">
+  
+  <h1>Curriculum Vitae</h1>
+  <h2>Données personnels</h2>
+  <p class="name">{{nom}} {{prenom}}</p> 
+  <p>{{adress}}</p> 
+  <p v-if="birthday !== null">{{age}} ans</p>
+  <p v-if="drive  == true" class="green"> Titulaire du permis B </p>
+  </div>
+  `,
+  props: ["nom", "prenom", "adress", "birthday", "drive"],
+  data: function () {
+    return {
 
     }
 
   },
   methods: {
+
+  },
+  computed: {
+
+    age: function () {
+
+      now = Date.now()
+      temp = now - Date.parse(this.birthday)
+      total = temp / 31536000000
+      return Math.floor(total);
+    }
 
 
   }
@@ -26,21 +137,102 @@ let vm = new Vue({
   /*Sélecteur. Même syntaxe que le CSS*/
   data: {
 
-    nom: null
+    nom: null,
+    prenom: null,
+    adress: null,
+    birthday: null,
+    drive: null,
+    schools: [],
+    jobs: [],
+    skills: []
+
 
 
   },
 
   components: {
 
-    "personal-cmp": personal
+    "personal-cmp": personal,
+    "formation-cmp": formation,
+    "exp-cmp": exp,
+    "skill-cmp": skill
 
   },
   methods: {
 
+    saveCv: function () {
+
+      //
+
+
+      localStorage.setItem("cv", JSON.stringify(this.$data))
+
+    },
+
+    addSchool: function () {
+
+
+      let label = document.getElementById("schoolLib").value
+      let start = document.getElementById("schoolStart").value
+      let end = document.getElementById("schoolEnd").value
+
+      let school = {
+        name: label,
+        start: start,
+        end: end
+      }
+
+      this.schools.push(school)
+
+      document.getElementById("schoolLib").value = ''
+      document.getElementById("schoolStart").value = ''
+      document.getElementById("schoolEnd").value = ''
+
+    },
+    addJob: function () {
+
+
+      let label = document.getElementById("jobLib").value
+      let start = document.getElementById("jobStart").value
+      let end = document.getElementById("jobEnd").value
+
+
+      let job = {
+        name: label,
+        start: start,
+        end: end
+      }
+
+      this.jobs.push(job)
+
+      document.getElementById("jobLib").value = ''
+      document.getElementById("jobStart").value = ''
+      document.getElementById("jobEnd").value = ''
+
+    },
+    addSkill: function () {
+
+
+      let label = document.getElementById("skillLib").value
+
+      let skill = {
+
+        name: label
+      }
+      this.skills.push(skill)
+      document.getElementById("skillLib").value = ''
+
+
+    }
 
 
 
-  }
+
+
+
+  },
+
+
+
 
 });
